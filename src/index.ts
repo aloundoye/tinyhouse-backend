d12a3d45d1bd1be82c595aa18f392f0e38efe4ef
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-01-10 00:53:36 +0000

    create listing and delete-listing routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { listings } from "./listings";
 const app = express();
 const port = 9000;
-const one = 1;
-const two = 2;
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use(bodyParser.json());
+
+app.get("/listings", (req, res) => {
+  return res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+
+  return res.send('failed to delete listing');
+});
 
 app.listen(port);
 
