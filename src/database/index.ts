6ddd44175c190a85f6f6a29da8e16c31fd5a6273
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-01-15 01:35:31 +0000

    adding types to Collection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,12 @@
 import { MongoClient } from "mongodb";
-
+import {Database} from "../lib/types";
 const user = "alou";
 const userPassword = "1pkGDX0nPb0l6UNQ";
 const cluster = "cluster0.yluko";
 
 const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url);
   const db = client.db("tinyhouse");
 
