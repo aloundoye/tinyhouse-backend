b028e487d0e239b804799fb9505e3e4c4c5d0371
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-02-08 01:22:11 +0000

    final code

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,37 +1,204 @@
-import { IResolvers } from "@graphql-tools/utils";
-import { Database, Listing } from "../../../lib/types";
+import { IResolvers } from "apollo-server-express";
+import { Request } from "express";
 import { ObjectId } from "mongodb";
+import { Cloudinary, Google } from "../../../lib/api";
+import { Database, Listing, ListingType, User } from "../../../lib/types";
+import { authorize } from "../../../lib/utils";
+import {
+  ListingArgs,
+  ListingBookingsArgs,
+  ListingBookingsData,
+  ListingsArgs,
+  ListingsData,
+  ListingsFilter,
+  ListingsQuery,
+  HostListingInput,
+  HostListingArgs
+} from "./types";
+
+const verifyHostListingInput = ({
+  title,
+  description,
+  type,
+  price
+}: HostListingInput) => {
+  if (title.length > 100) {
+    throw new Error("listing title must be under 100 characters");
+  }
+  if (description.length > 5000) {
+    throw new Error("listing description must be under 5000 characters");
+  }
+  if (type !== ListingType.Apartment && type !== ListingType.House) {
+    throw new Error("listing type must be either an apartment or house");
+  }
+  if (price < 0) {
+    throw new Error("price must be greater than 0");
+  }
+};
 
 export const listingResolvers: IResolvers = {
   Query: {
-    listings: async (
-      root: undefined,
-      args: {},
-      { db }: { db: Database }
-    ): Promise<Listing[]> => {
-      return await db.listings.find({}).toArray();
-    },
-  },
-
-  Mutation: {
-    deleteListing: async (
-      root: undefined,
-      { id }: { id: string },
-      { db }: { db: Database }
+    listing: async (
+      _root: undefined,
+      { id }: ListingArgs,
+      { db, req }: { db: Database; req: Request }
     ): Promise<Listing> => {
-      const deleteResult = await db.listings.findOneAndDelete({
-        _id: new ObjectId(id),
-      });
+      try {
+        const listing = await db.listings.findOne({ _id: new ObjectId(id) });
+        if (!listing) {
+          throw new Error("listing can't be found");
+        }
 
-      if (!deleteResult.value) {
-        throw new Error("failed to delete listing");
+        const viewer = await authorize(db, req);
+        if (viewer && viewer._id === listing.host) {
+          listing.authorized = true;
+        }
+
+        return listing;
+      } catch (error) {
+        throw new Error(`Failed to query listing: ${error}`);
+      }
+    },
+    listings: async (
+      _root: undefined,
+      { location, filter, limit, page }: ListingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingsData> => {
+      try {
+        const query: ListingsQuery = {};
+        const data: ListingsData = {
+          region: null,
+          total: 0,
+          result: []
+        };
+
+        if (location) {
+          const { country, admin, city } = await Google.geocode(location);
+
+          if (city) query.city = city;
+          if (admin) query.admin = admin;
+          if (country) {
+            query.country = country;
+          } else {
+            throw new Error("no country found");
+          }
+
+          const cityText = city ? `${city}, ` : "";
+          const adminText = admin ? `${admin}, ` : "";
+          data.region = `${cityText}${adminText}${country}`;
+        }
+
+        let cursor = await db.listings.find(query);
+
+        if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+          cursor = cursor.sort({ price: 1 });
+        }
+
+        if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+          cursor = cursor.sort({ price: -1 });
+        }
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query listings: ${error}`);
+      }
+    }
+  },
+  Mutation: {
+    hostListing: async (
+      _root: undefined,
+      { input }: HostListingArgs,
+      { db, req }: { db: Database; req: Request }
+    ): Promise<Listing> => {
+      verifyHostListingInput(input);
+
+      let viewer = await authorize(db, req);
+      if (!viewer) {
+        throw new Error("viewer cannot be found");
       }
 
-      return deleteResult.value;
-    },
-  },
+      const { country, admin, city } = await Google.geocode(input.address);
+      if (!country || !admin || !city) {
+        throw new Error("invalid address input");
+      }
 
-  Listing: {
-    id: (listing: Listing): string => listing._id.toString(),
+      const imageUrl = await Cloudinary.upload(input.image);
+
+      const insertResult = await db.listings.insertOne({
+        _id: new ObjectId(),
+        ...input,
+        image: imageUrl,
+        bookings: [],
+        bookingsIndex: {},
+        country,
+        admin,
+        city,
+        host: viewer._id
+      });
+
+      const insertedListing: Listing = insertResult.ops[0];
+
+      await db.users.updateOne(
+        { _id: viewer._id },
+        { $push: { listings: insertedListing._id } }
+      );
+
+      return insertedListing;
+    }
   },
+  Listing: {
+    id: (listing: Listing): string => {
+      return listing._id.toString();
+    },
+    host: async (
+      listing: Listing,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<User> => {
+      const host = await db.users.findOne({ _id: listing.host });
+      if (!host) {
+        throw new Error("host can't be found");
+      }
+      return host;
+    },
+    bookingsIndex: (listing: Listing): string => {
+      return JSON.stringify(listing.bookingsIndex);
+    },
+    bookings: async (
+      listing: Listing,
+      { limit, page }: ListingBookingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingBookingsData | null> => {
+      try {
+        if (!listing.authorized) {
+          return null;
+        }
+
+        const data: ListingBookingsData = {
+          total: 0,
+          result: []
+        };
+
+        let cursor = await db.bookings.find({
+          _id: { $in: listing.bookings }
+        });
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query listing bookings: ${error}`);
+      }
+    }
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 
 import express, { Application } from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
 import { ApolloServer } from "apollo-server-express";
-import { typeDefs, resolvers } from "./graphql";
 import { connectDatabase } from "./database";
+import { typeDefs, resolvers } from "./graphql";
 
-
-const startApolloServer = async (app: Application) => {
+const mount = async (app: Application) => {
   const db = await connectDatabase();
+
+  app.use(bodyParser.json({ limit: "2mb" }));
+  app.use(cookieParser(process.env.SECRET));
+  app.use(compression());
+
+  // app.use(express.static(`${__dirname}/client`));
+  // app.get("/*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
 
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({ req, res }) => ({ db, req, res })
   });
-  await server.start();
-  server.applyMiddleware({ app, path: "/api" });
 
+  server.applyMiddleware({ app, path: "/api" });
   app.listen(process.env.PORT);
-  console.log(`[app]: http://localhost:${process.env.PORT}`);
+
+  console.log(`[app] : http://localhost:${process.env.PORT}`);
 };
 
-startApolloServer(express());
+mount(express());
