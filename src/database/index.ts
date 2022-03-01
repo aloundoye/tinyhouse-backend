5b5eabc4c266d6703681c49ccc24a394bb7115ca
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-03-01 16:04:42 +0000

    fix mongodb new string problem

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import { Database } from "../lib/types";
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const url = `mongodb://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net:27017,${process.env.DB_CLUSTER}.mongodb.net:27017,${process.env.DB_CLUSTER}.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-esc9xp-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url);
