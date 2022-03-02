7262c2978e61f0f9e361c69d07f8266670035129
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-03-02 15:18:16 +0000

    seed and clear data from mongodb

---
+++ temp/clear.ts
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+import { ObjectId } from "mongodb";
+import { connectDatabase } from "../src/database";
+import { Listing, ListingType, User } from "../src/lib/types";
+
+const clear = async () => {
+  try {
+    console.log("[clear] : running...");
+    const db = await connectDatabase();
+
+    const bookings = await db.bookings.find({}).toArray();
+    const listings = await db.listings.find({}).toArray();
+    const users = await db.users.find({}).toArray();
+
+    if (bookings.length > 0) {
+      await db.bookings.drop();
+    }
+
+    if (listings.length > 0) {
+      await db.listings.drop();
+    }
+
+    if (users.length > 0) {
+      await db.users.drop();
+    }
+
+    console.log("[clear]: success");
+  } catch (error) {
+    throw new Error("failed to clear database");
+  }
+};
+
+clear();
