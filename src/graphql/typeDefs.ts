05db3a7f1d1153e7a3c34eee24382cc0a2be63d7
Alassane Ndoye <aloutndoye.an@gmail.com>
2022-03-04 22:21:59 +0000

    Building the Authentication Resolvers

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,12 +1,23 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
+  type Viewer {
+    id: ID
+    token: String
+    avatar: String
+    hasWallet: Boolean!
+  }
+
+  input LogInput {
+    code: String!
+  }
+
   type Query {
     authUrl: String!
   }
 
   type Mutation {
-    logIn: String!
+    logIn(input: LogInput): String!
     logOut: String!
   }
 `;
--- src/lib/api/Google.ts
+++ src/lib/api/Google.ts
@@ -11,7 +11,7 @@ export const Google = {
     access_type: "online",
     scope: [
       "https://www.googleapis.com/auth/userinfo.email",
-      "https://www.googleapis/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.profile",
     ],
   }),
   logIn: async (code: string) => {
